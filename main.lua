7973841749022b2092656439766c9ccc508522df
feelixe <felixeklof@gmail.com>
2014-05-13 15:22:34 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 function love.load()
-  img = love.graphics.newImage("DSC_0203.JPEG")
   player = {
     x = 0;
     y = 0;
@@ -24,5 +23,4 @@ end
 
 function love.draw()
   love.graphics.rectangle("fill",player.x,player.y,50,50) 
-  love.graphics.draw(img, 100, 20)
 end
