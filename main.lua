fff0a6f2ce8f5222f53ff0aeac8c7d1baff4746e
Kittyloaf <hongkongdelivery@chinamail.com>
2014-05-13 15:15:01 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 function love.load()
+  img = love.graphics.newImage(https://cloud.githubusercontent.com/assets/7569042/2957475/0ad46cf4-daa0-11e3-849a-af3f79ceb1ec.gif)
   player = {
     x = 0;
     y = 0;
@@ -12,4 +13,5 @@ end
 
 function love.draw()
   love.graphics.rectangle("fill",player.x,player.y,50,50) 
+  love.graphics.draw(img, 100, 20)
 end
